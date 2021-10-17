c96718331a134c04f95389fb13951c087f023551
Hui-Chech <huichechchang@gmail.com>
2021-10-17 22:47:29 +0800

    將專案放入

---
+++ Ox_Game/Assets/Script/Ox_Control.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ox_Control : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed = 0;
+
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        Vector3 move = new Vector3(1, 0, 0);
+        move.x = transform.position.x - Speed * Time.deltaTime;
+        rigidbody.MovePosition(move);
+    }
+}
